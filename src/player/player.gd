99b4268c415be5dab339b0540ead264d0e44c83f
Zhyrin <zhyrin@tutanota.com>
2022-04-20 20:38:26 +0200

    Player can shoot projectiles

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,16 +1,43 @@
 extends KinematicBody2D
 
+const Projectile := preload("res://src/projectile/projectile.gd")
+const ProjectileScene := preload("res://src/projectile/projectile.tscn")
+
 
 const PLATFORM_COLLISION_BIT := 3
 export(int) var movement_speed: int
 export(int) var jump_height: int setget set_jump_height
 export(float) var jump_time: float = 1.0 setget set_jump_time
 export(float) var fall_gravity_multiplier: float = 1.0
+export(float) var strong_charge: float
 var jump_velocity: float
 var gravity: float
 var velocity := Vector2.ZERO
 var fallthrough_platform: Node = null
+var attack_charge := 0.0
+var charge_audio_triggered := false
 onready var platform_raycast := $PlatformRayCast as RayCast2D
+onready var projectile_pos := $ProjectilePosition as Position2D
+onready var attack_charge_audio := $AttackChargeAudio as AudioStreamPlayer
+onready var attack_cooldown := $AttackCooldown as Timer
+
+
+func _process(delta: float) -> void:
+	var can_attack := attack_cooldown.is_stopped()
+	if can_attack and Input.is_action_pressed("attack"):
+		attack_charge += delta
+		if attack_charge >= strong_charge and not charge_audio_triggered:
+			attack_charge_audio.play()
+			charge_audio_triggered = true
+	if can_attack and Input.is_action_just_released("attack"):
+		var projectile := ProjectileScene.instance() as Projectile
+		projectile.init(projectile_pos.global_position,
+				(get_global_mouse_position() - projectile_pos.global_position).normalized(),
+				attack_charge >= strong_charge)
+		get_parent().add_child(projectile)
+		attack_charge = 0.0
+		attack_cooldown.start()
+		charge_audio_triggered = false
 
 
 func _physics_process(delta: float) -> void:
