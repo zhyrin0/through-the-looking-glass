780911ce9659acbf139d806cb3c370df2b9fdd53
Zhyrin <zhyrin@tutanota.com>
2022-04-24 18:14:05 +0200

    Fix shard break off direction

--- src/character/player/shard/shard.gd
+++ src/character/player/shard/shard.gd
@@ -11,13 +11,13 @@ func _physics_process(delta: float) -> void:
 
 
 func break_off() -> void:
+	direction = position.normalized()
 	var tmp_global_pos := global_position
 	# note: Level > Player > Shards
 	var new_parent := get_parent().get_parent().get_parent()
 	get_parent().remove_child(self)
 	new_parent.add_child(self)
 	global_position = tmp_global_pos
-	direction = position.normalized()
 	var tween := $Tween as Tween
 	tween.interpolate_property(self, "modulate", null, Color(1.0, 1.0, 1.0, 0.0),
 		decay_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
