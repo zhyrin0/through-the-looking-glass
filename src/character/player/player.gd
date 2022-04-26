460c4bc9c6e0ed44f85bdccc3410a231e2dde8f9
Zhyrin <zhyrin@tutanota.com>
2022-04-26 18:27:44 +0200

    Add player animations

--- src/character/player/player.gd
+++ src/character/player/player.gd
@@ -3,32 +3,34 @@ extends "../character_base.gd"
 const Shard := preload("shard/shard.gd")
 
 
+enum AttackState {
+	NOT_ATTACKING,
+	STARTING,
+	CHARGING,
+	RELEASING,
+}
+
 export(int) var max_health: int
 export(float) var strong_charge: float
 var attack_charge := 0.0
+var use_strong_attack := false
 var charge_audio_triggered := false
 var score := 0
+var lock_animation := false
 onready var health := max_health
-onready var shards := $Shards as Node
+onready var shards := $Pivot/Shards as Node
 onready var charge_audio := $ChargeAudio as AudioStreamPlayer
 onready var attack_cooldown := $AttackCooldown as Timer
 
 
+func _ready() -> void:
+	animation_player.play("idle")
+
+
 func _process(delta: float) -> void:
-	var can_attack := attack_cooldown.is_stopped()
-	if can_attack:
-		if Input.is_action_pressed("attack"):
-			attack_charge += delta
-			if attack_charge >= strong_charge and not charge_audio_triggered:
-				charge_audio_triggered = true
-				charge_audio.play()
-		elif Input.is_action_just_released("attack"):
-			attack()
-			attack_cooldown.start()
-			attack_charge = 0.0
-			charge_audio_triggered = false
-	
+	var attack_state := attack_logic(delta)
 	pivot.scale.x = sign(get_local_mouse_position().x)
+	set_animation(attack_state)
 
 
 func _physics_process(delta: float) -> void:
@@ -46,6 +48,46 @@ func _physics_process(delta: float) -> void:
 	reset_collision_rules()
 
 
+func attack_logic(delta: float) -> int:
+	var result: int = AttackState.NOT_ATTACKING
+	var can_attack := attack_cooldown.is_stopped()
+	if can_attack:
+		if Input.is_action_pressed("attack"):
+			result = AttackState.STARTING if attack_charge == 0.0 else AttackState.CHARGING
+			attack_charge += delta
+			if attack_charge >= strong_charge and not charge_audio_triggered:
+				charge_audio_triggered = true
+				charge_audio.play()
+		elif Input.is_action_just_released("attack"):
+			result = AttackState.RELEASING
+			attack_cooldown.start()
+			use_strong_attack = attack_charge >= strong_charge
+			attack_charge = 0.0
+			charge_audio_triggered = false
+	return result
+
+
+func set_animation(attack_state: int) -> void:
+	match attack_state:
+		AttackState.STARTING:
+			animation_player.play("attack_charge")
+			lock_animation = true
+		AttackState.RELEASING:
+			animation_player.play("attack_release")
+	
+	if lock_animation:
+		return
+	var anim := animation_player.current_animation
+	if velocity.y < 0.0 and anim != "jump":
+		animation_player.play("jump")
+	elif velocity.y > 0.0 and anim != "fall":
+		animation_player.play("fall")
+	elif velocity.x != 0.0 and anim != "run" and is_on_floor():
+		animation_player.play("run")
+	elif velocity.x == 0.0 and anim != "idle" and is_on_floor():
+		animation_player.play("idle")
+
+
 func on_hit() -> void:
 	health -= 1
 	if shards.get_child_count() > 0:
@@ -56,8 +98,13 @@ func on_hit() -> void:
 
 
 func attack() -> void:
-	_attack(Projectile.Owner.PLAYER, get_global_mouse_position(), attack_charge >= strong_charge)
+	_attack(Projectile.Owner.PLAYER, get_global_mouse_position(), use_strong_attack)
 
 
 func _on_Enemy_hit() -> void:
 	score += 1
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name == "attack_release":
+		lock_animation = false
